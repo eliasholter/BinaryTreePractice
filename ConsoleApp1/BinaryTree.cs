fe717d8fc3114d910e324e99e8496b2d3fd549bc
Elias Holter <eliasholter1@gmail.com>
2019-12-11 16:10:30 -0600

    Initial commit, both functions completed

---
+++ ConsoleApp1/BinaryTree.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreeProject
+{
+    class BinaryTree
+    {
+        public Node root;
+
+        public BinaryTree()
+        {
+            root = null;
+        }
+
+        public void AddItem(int data)
+        {
+            Node node = new Node(data);
+
+            // If no start node exists, set this as the start node
+            if(root == null)
+            {
+                root = node;
+                return;
+            }
+
+            // If there is already a value in the tree, recursively check for the proper spot to insert the new Node
+            RecursiveAdd(root, node);            
+        }
+
+        public static void RecursiveAdd(Node original, Node data)
+        {
+            if(data.data < original.data)
+            {
+                if(original.right == null)
+                {
+                    original.right = data;
+                }
+                else
+                {
+                    RecursiveAdd(original.right, data);
+                }
+            }
+            else
+            {
+                if (original.left == null)
+                {
+                    original.left = data;
+                }
+                else
+                {
+                    RecursiveAdd(original.left, data);
+                }
+            }
+
+        }
+
+        public string SearchForNode(Node original, Node nodeOfInterest)
+        {
+            Node compareTo = original;
+
+            if(compareTo.data == nodeOfInterest.data)
+            {
+                return nodeOfInterest.data.ToString();
+            }
+            if (compareTo == null)
+            {
+                return null;
+            }
+            if (compareTo.data > nodeOfInterest.data)
+            {
+                return SearchForNode(compareTo.right, nodeOfInterest);
+            }
+            else
+            {
+                return SearchForNode(compareTo.left, nodeOfInterest);
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreeProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree tree = new BinaryTree();
+            Node checker = new Node(30);
+
+            tree.AddItem(50);
+            tree.AddItem(45);
+            tree.AddItem(55);
+            tree.AddItem(60);
+            tree.AddItem(30);
+            tree.AddItem(5);
+            Console.WriteLine(tree.SearchForNode(tree.root, checker));
+            Console.ReadLine();
+
+        }
+    }
+}
+
+
